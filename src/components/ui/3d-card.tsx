cb67a8fd38913ce3a20d427aee5c6519a80165ff
dedsec <152778879+sddion@users.noreply.github.com>
2026-03-18 23:49:25 +0530

    refactor: Move home page client-side logic and UI from `page.tsx` to a new `HomeClient` component.

--- src/components/ui/3d-card.tsx
+++ src/components/ui/3d-card.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { cn } from "@/app/lib/utils";
+import { cn } from "@/lib/utils";
 
 import React, {
   createContext,
@@ -86,7 +86,7 @@ export const CardBody = ({
   return (
     <div
       className={cn(
-        "h-96 w-96 [transform-style:preserve-3d]  [&>*]:[transform-style:preserve-3d]",
+        "h-96 w-96 transform-3d  *:transform-3d",
         className
       )}
     >
